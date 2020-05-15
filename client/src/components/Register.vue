f3803f8de847ccdd432f2890012753506ef4da55
Alexander Shatravka <shatravgo2@gmail.com>
2020-05-16 02:06:18 +0300

    set up registration backend and add vuetify lib

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,23 +1,33 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email">
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password">
-    <br>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan">
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email">
+          <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password">
+          <br>
+          <div v-html="error" class="error"/>
+          <v-btn
+            class="cyan"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -27,16 +37,21 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -44,5 +59,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .error {
+    color: red;
+  }
 </style>
